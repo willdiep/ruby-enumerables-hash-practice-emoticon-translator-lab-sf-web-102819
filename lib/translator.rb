e066693b1657bfd79ef05916a0fe0096ba27a42a
Will Diep <widiep@gmail.com>
2019-10-04 04:38:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,24 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(yaml_file_path)
+    yaml_file_path = YAML.load_file(yaml_file_path)
+    hash = {
+        "get_meaning" => {},
+        "get_emoticon" => {}
+    }
+    yaml_file_path.each do |meaning, array_emoticons| 
+        # p array_emoticons
+        hash["get_meaning"][array_emoticons[1]] = meaning
+        hash["get_emoticon"][array_emoticons[0]] = array_emoticons[1]
+
+        # array_emoticons.each do |emoticons|
+        #     p emoticons
+        # end
+    end
+    hash
 end
+
+
 
 def get_japanese_emoticon
   # code goes here
